4d19105e3d6b315b6638f28308717ceb82c06c0a
Ericka Ponce <erickaponce92@gmail.com>
2025-01-28 13:27:06 -0500

    add useState and validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,34 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import { countries } from "../data/countries";
+import { SearchType } from "../types";
 import styles from './Form.module.css'
 
 export default function Form() {
+    const [search, setSearch] = useState<SearchType>({
+        city: '',
+        country: ''
+    });
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if(Object.values(search).includes('')) {
+            console.log('Hay campos vacios');
+        }
+    }
+
     return (
-        <form className={styles.form}>
+        <form 
+            className={styles.form}
+            onSubmit={handleSubmit}
+        >
             <div className={styles.field}>
                 <label htmlFor="city">Ciudad</label>
                 <input 
@@ -11,11 +36,18 @@ export default function Form() {
                     type="text"
                     name="city"
                     placeholder="Ciudad"
+                    value={search.city}
+                    onChange={handleChange}
                 />
             </div>
             <div className={styles.field}>
                 <label htmlFor="country">País</label>
-                <select>
+                <select 
+                    id="country" 
+                    value={search.country}
+                    name="country"
+                    onChange={handleChange}
+                >
                     <option value="">--Seleccione un país--</option>
                     {countries.map(country => (
                         <option 
